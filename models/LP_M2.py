d566aa2e32445d6b027ade935c2b58a4d9c2653e
Martin <martin.schoenstein@etu.unistra.fr>
2026-04-07 14:35:27 +0200

    settings in code, seconds in output file, opti double_split and new double_split2 (create neg only in cc that can get the full good amount of neg)

--- models/LP_M2.py
+++ models/LP_M2.py
@@ -1,5 +1,5 @@
-#!/home/schoenstein/.conda/envs/graphe/bin/python
-#SBATCH --job-name=LP_M2
+#!/home/schoenstein/.conda/envs/gnn/bin/python
+#SBATCH --job-name=LP_M2_2
 #SBATCH --output=/home/schoenstein/these/slurm_out/slurm-%J.out --error=/home/schoenstein/these/slurm_out/slurm-%J.err
 
 
@@ -16,19 +16,42 @@ from torch_geometric.loader import LinkNeighborLoader
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
 from datetime import datetime
+import sys
+import random
+from torch_geometric.utils import negative_sampling
+import time
 
 
+start = time.time()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 
-with open("/home/schoenstein/these/models/M2_settings.json", "r") as file:
-    settings = json.load(file)
+#with open("/home/schoenstein/these/models/M2_settings.json", "r") as file:
+#    settings = json.load(file)
 
-
+settings = {"graph": "/home/schoenstein/these/graph/graph_light.gml",
+    "options": {
+            "negative_sampling": "double split2",
+            "negative_sampling_ratio": 0.5,              #1.5 our obtenir 0.8 en double split1
+            "attributes": "statistics",
+            "aggr": "lstm",
+            "hidden_channels": 64,
+            "num_neighbors": [25, 10],
+            "batch_size": 128,
+            "lr": 0.01,
+            "early_stop": 6
+        },
+    "output": "/home/schoenstein/these/models/training/"
+        }
 
 G = nx.read_gml(settings["graph"])
+node_to_idx = {}
+for i, n in enumerate(G.nodes()):
+    node_to_idx[n] = i
+    G.nodes[n]['id'] = i
+G = nx.relabel_nodes(G, node_to_idx)
 data = from_networkx(G)
 
 
@@ -46,25 +69,33 @@ if settings["options"]["negative_sampling"] == "random":
         is_undirected = True
     )
     train_data, val_data, test_data = transform(data)
-
-elif settings["options"]["negative_sampling"] == "double split":
-    cc = list(nx.connected_components(G))
+elif settings["options"]["negative_sampling"] == "double split1":
+    cc = data.connected_components()
     neg_inside = 0
+    neg_inside_val = 0
     train_list = []
     val_list = []
     test_list = []
+    negative_sampling_ratio = settings["options"]["negative_sampling_ratio"]
     for c in cc:
-        G2 = G.subgraph(c).copy()
-        data2 = from_networkx(G2)
         transform = T.RandomLinkSplit(
                 num_val = 0.1,  
                 num_test = 0.1,  
                 disjoint_train_ratio = 0,  
-                neg_sampling_ratio = 1.5,
+                neg_sampling_ratio = negative_sampling_ratio,
                 is_undirected = True
             )
-        train_data2, val_data2, test_data2 = transform(data2)
-        neg_inside = neg_inside + len(train_data2.edge_label)
+        train_data2, val_data2, test_data2 = transform(c)
+        train_data2.edge_index = train_data2.id[train_data2.edge_index]
+        test_data2.edge_index = test_data2.id[test_data2.edge_index]
+        val_data2.edge_index = val_data2.id[val_data2.edge_index]
+        train_data2.edge_label_index = train_data2.id[train_data2.edge_label_index]
+        val_data2.edge_label_index = val_data2.id[val_data2.edge_label_index]
+        test_data2.edge_label_index = test_data2.id[test_data2.edge_label_index]
+        del train_data2.id
+        del val_data2.id
+        del test_data2.id
+        neg_inside = neg_inside + (train_data2.edge_label == 0).sum().item()
         train_list.append(train_data2)
         val_list.append(val_data2)
         test_list.append(test_data2)
@@ -78,17 +109,18 @@ elif settings["options"]["negative_sampling"] == "double split":
             is_undirected = True
         )
     train_data3, val_data3, test_data3 = transform(data)
+
     pos_train = train_data3.edge_label_index[:, train_data3.edge_label == 1]
     pos_val = val_data3.edge_label_index[:, val_data3.edge_label == 1]
     pos_test = test_data3.edge_label_index[:, test_data3.edge_label == 1]
     neg_train1 = torch.cat([d.edge_label_index[:, d.edge_label==0] 
-                             for d in train_list], dim=1)
+                             for d in train_list], dim=1) #neg intra cc
     neg_val1 = torch.cat([d.edge_label_index[:, d.edge_label==0] 
                            for d in val_list], dim=1)
     neg_test1 = torch.cat([d.edge_label_index[:, d.edge_label==0] 
                             for d in test_list], dim=1)
-    neg_train2 = train_data3.edge_label_index[:, train_data3.edge_label==0]             
-    neg_val2 = val_data3.edge_label_index[:, val_data3.edge_label==0] 
+    neg_train2 = train_data3.edge_label_index[:, train_data3.edge_label==0]           
+    neg_val2 = val_data3.edge_label_index[:, val_data3.edge_label==0]
     neg_test2 = test_data3.edge_label_index[:, test_data3.edge_label==0]
     neg_train = torch.cat([neg_train1, neg_train2], dim=1)
     neg_val = torch.cat([neg_val1, neg_val2], dim=1)
@@ -120,7 +152,243 @@ elif settings["options"]["negative_sampling"] == "double split":
             torch.zeros(neg_test.size(1), dtype=torch.long)
         ])
     )
+elif settings["options"]["negative_sampling"] == "double split2":
+    cc = data.connected_components()
+    neg_inside = 0
+    neg_inside_val = 0
+    train_list = []
+    val_list = []
+    test_list = []
+    negative_sampling_ratio = settings["options"]["negative_sampling_ratio"]
+    print("negative_sampling_ratio : ", negative_sampling_ratio)
+    for c in cc:
+        num_nodes = int(c.num_nodes)
+        num_edges = int(c.num_edges)
+        if (num_nodes * (num_nodes -1))/2 - num_edges/2 > num_edges/2 * negative_sampling_ratio:
+            transform = T.RandomLinkSplit(
+                    num_val = 0.1,  
+                    num_test = 0.1,  
+                    disjoint_train_ratio = 0,  
+                    neg_sampling_ratio = negative_sampling_ratio,
+                    is_undirected = True
+                )
+        else:
+            transform = T.RandomLinkSplit(
+                    num_val = 0.1,  
+                    num_test = 0.1,  
+                    disjoint_train_ratio = 0,  
+                    neg_sampling_ratio = 0,
+                    is_undirected = True
+                )
+        train_data2, val_data2, test_data2 = transform(c)
+        train_data2.edge_index = train_data2.id[train_data2.edge_index]
+        test_data2.edge_index = test_data2.id[test_data2.edge_index]
+        val_data2.edge_index = val_data2.id[val_data2.edge_index]
+        train_data2.edge_label_index = train_data2.id[train_data2.edge_label_index]
+        val_data2.edge_label_index = val_data2.id[val_data2.edge_label_index]
+        test_data2.edge_label_index = test_data2.id[test_data2.edge_label_index]
+        del train_data2.id
+        del val_data2.id
+        del test_data2.id
+        neg_inside = neg_inside + (train_data2.edge_label == 0).sum().item()
+        neg_total_goal = int(neg_inside/negative_sampling_ratio)
+        neg_inside_val = neg_inside_val + (val_data2.edge_label == 0).sum().item()
+        neg_total_goal_val = int(neg_inside_val/negative_sampling_ratio)
+        train_list.append(train_data2)
+        val_list.append(val_data2)
+        test_list.append(test_data2)
+    ratio_neg_inside = neg_inside/(len(list(G.edges()))*2)
+    print("Ratio neg intra cc : ", ratio_neg_inside)
+    transform = T.RandomLinkSplit(
+            num_val = 0.1,  
+            num_test = 0.1,  
+            disjoint_train_ratio = 0,  
+            neg_sampling_ratio = 1 - negative_sampling_ratio,
+            is_undirected = True
+        )
+    train_data3, val_data3, test_data3 = transform(data)
 
+    pos_train = train_data3.edge_label_index[:, train_data3.edge_label == 1]
+    n_pos_train = pos_train.size(1)
+    perm = torch.randperm(n_pos_train)[:neg_total_goal]
+    pos_train = pos_train[:, perm] #Sous selection des positives du train
+
+    neg_train = train_data3.edge_label_index[:, train_data3.edge_label == 0]
+    n_neg_train = neg_train.size(1)
+    perm_neg = torch.randperm(n_neg_train)[:int(neg_total_goal*(1 - negative_sampling_ratio))]
+    neg_train = neg_train[:, perm_neg] #Sous selection des negatives globales du train
+
+    print(neg_inside, neg_train.size(1), pos_train.size(1), n_pos_train)
+
+    pos_val = val_data3.edge_label_index[:, val_data3.edge_label == 1]
+    n_pos_val = pos_val.size(1)
+    perm_val = torch.randperm(n_pos_val)[:neg_total_goal_val]
+    pos_val = pos_val[:, perm_val] #val
+
+    neg_val = val_data3.edge_label_index[:, val_data3.edge_label == 0]
+    n_neg_val = neg_val.size(1)
+    perm_neg_val = torch.randperm(n_neg_val)[:int(neg_total_goal_val*(1 - negative_sampling_ratio))]
+    neg_val = neg_val[:, perm_neg_val] #val
+
+    pos_test = test_data3.edge_label_index[:, test_data3.edge_label == 1]
+    neg_train1 = torch.cat([d.edge_label_index[:, d.edge_label==0] 
+                             for d in train_list], dim=1) #neg intra cc
+    neg_val1 = torch.cat([d.edge_label_index[:, d.edge_label==0] 
+                           for d in val_list], dim=1)
+    neg_test1 = torch.cat([d.edge_label_index[:, d.edge_label==0] 
+                            for d in test_list], dim=1)
+
+    neg_train2 = neg_train           
+    neg_val2 = neg_val 
+    neg_test2 = test_data3.edge_label_index[:, test_data3.edge_label==0]
+    neg_train = torch.cat([neg_train1, neg_train2], dim=1)
+    neg_val = torch.cat([neg_val1, neg_val2], dim=1)
+    neg_test = torch.cat([neg_test1, neg_test2], dim=1)
+    train_data = Data(
+        edge_index=train_data3.edge_index,
+        num_nodes=data.num_nodes,
+        edge_label_index=torch.cat([pos_train, neg_train], dim=1),
+        edge_label=torch.cat([
+            torch.ones(pos_train.size(1), dtype=torch.long),
+            torch.zeros(neg_train.size(1), dtype=torch.long)
+        ])
+    )
+    val_data = Data(
+        edge_index=val_data3.edge_index,
+        num_nodes=data.num_nodes,
+        edge_label_index=torch.cat([pos_val, neg_val], dim=1),
+        edge_label=torch.cat([
+            torch.ones(pos_val.size(1), dtype=torch.long),
+            torch.zeros(neg_val.size(1), dtype=torch.long)
+        ])
+    )
+    test_data = Data(
+        edge_index=test_data3.edge_index,
+        num_nodes=data.num_nodes,
+        edge_label_index=torch.cat([pos_test, neg_test], dim=1),
+        edge_label=torch.cat([
+            torch.ones(pos_test.size(1), dtype=torch.long),
+            torch.zeros(neg_test.size(1), dtype=torch.long)
+        ])
+    )
+# --------------  NOT READY -----------------    
+if settings["options"]["negative_sampling"] == "cc":
+    cc = list(nx.connected_components(G))
+    random.seed(42)
+    random.shuffle(cc)
+    cut_train = 0.1
+    cut_val = 0.1
+    cut_test = 0.8
+    cc_train = cc[: int(len(cc)*cut_train)]
+    cc_val = cc[int(len(cc)*cut_train) : int(len(cc)*(cut_train + cut_val))]
+    cc_test = cc[int(len(cc)*(cut_train + cut_val)) :]
+    cc_train_nodes = []
+    for s in cc_train:
+        for n in s:
+            cc_train_nodes.append(n)
+    cc_val_nodes = []
+    for s in cc_val:
+        for n in s:
+            cc_val_nodes.append(n)
+    #G_train = G.subgraph(cc_train_nodes).copy()
+    #data_train = from_networkx(G_train)
+    data_train = data.subgraph(cc_train_nodes) #remplace les deux au dessus normalement NE MARCHE PAS
+    negs_train = []
+    neg_inside_count = 0
+    for c in cc_train:
+        #G2_train = G_train.subgraph(c).copy() #mais ducoup il manque G_train ici...
+        #G2_train = G.subgraph(c).copy() option 1
+        #data2_train = from_networkx(G2_train) option 1 
+        data2_train = data.subgraph(list(c)) #option 2
+        neg_train_inside = negative_sampling(edge_index = data2_train.edge_index,
+                                    num_neg_samples =  int(data2_train.edge_index.size(1) * 1.5))
+        neg_inside_count = neg_inside_count + neg_train_inside.size(1) // 2
+        negs_train.append(neg_train_inside)
+    ratio_neg_inside = neg_inside_count/(data_train.edge_index.size(1) // 2)
+    print("Ratio neg intra cc : ", ratio_neg_inside)
+    neg_train_outside = negative_sampling(edge_index = data_train.edge_index,
+                                        num_neg_samples = int(data_train.edge_index.size(1) * (1 - ratio_neg_inside)))
+
+    train_pos_edge_index = data_train.edge_index
+    train_neg_edge_index = torch.cat(negs_train + [neg_train_outside], dim=1)
+    train_edge_label_index = torch.cat([train_pos_edge_index, train_neg_edge_index], dim=1)
+    train_edge_label = torch.cat([
+        torch.ones(train_pos_edge_index.size(1)),
+        torch.zeros(train_neg_edge_index.size(1))
+    ])
+    train_data = Data(
+        edge_index = train_pos_edge_index,              
+        edge_label_index = train_edge_label_index,      
+        edge_label = train_edge_label,
+        num_nodes = data_train.num_nodes,                
+    )
+    #G_val = G.subgraph(cc_val_nodes).copy()
+    #data_val = from_networkx(G_val)
+    data_val = data.subgraph(cc_val_nodes)
+    negs_val = []
+    neg_inside_count = 0
+    for c in cc_val:
+        G2_val = G_val.subgraph(c).copy()
+        data2_val = from_networkx(G2_val)
+        neg_val_inside = negative_sampling(edge_index = data2_val.edge_index,
+                                    num_neg_samples =  int(data2_val.edge_index.size(1) * 1.5))
+        neg_inside_count = neg_inside_count + neg_val_inside.size(1) // 2
+        negs_val.append(neg_val_inside)
+    ratio_neg_inside = neg_inside_count/(data_val.edge_index.size(1) // 2)
+    print("Ratio neg intra cc : ", ratio_neg_inside)
+    neg_val_outside = negative_sampling(edge_index = data_val.edge_index,
+                                        num_neg_samples = int(data_val.edge_index.size(1) * (1 - ratio_neg_inside)))
+    val_pos_edge_index = data_val.edge_index
+    val_neg_edge_index = torch.cat(negs_val + [neg_val_outside], dim=1)
+    val_edge_label_index = torch.cat([val_pos_edge_index, val_neg_edge_index], dim=1)
+    val_edge_label = torch.cat([
+        torch.ones(val_pos_edge_index.size(1)),
+        torch.zeros(val_neg_edge_index.size(1))
+    ])
+    val_data = Data(
+        edge_index = val_pos_edge_index,              
+        edge_label_index = val_edge_label_index,      
+        edge_label = val_edge_label,
+        num_nodes = data_val.num_nodes,                
+    )
+    cc_test_nodes = []
+    for s in cc_test:
+        for n in s:
+            cc_test_nodes.append(n)
+    cc_test_nodes = []
+    for s in cc_test:
+        for n in s:
+            cc_test_nodes.append(n)
+    #G_test = G.subgraph(cc_test_nodes).copy()
+    #data_test = from_networkx(G_test)
+    data_test = data.subgraph(cc_test_nodes)
+    negs_test = []
+    neg_inside_count = 0
+    for c in cc_test:
+        G2_test = G_test.subgraph(c).copy()
+        data2_test = from_networkx(G2_test)
+        neg_test_inside = negative_sampling(edge_index = data2_test.edge_index,
+                                    num_neg_samples =  int(data2_test.edge_index.size(1) * 1.5))
+        neg_inside_count = neg_inside_count + neg_test_inside.size(1) // 2
+        negs_test.append(neg_test_inside)
+    ratio_neg_inside = neg_inside_count/(data_test.edge_index.size(1) // 2)
+    print("Ratio neg intra cc : ", ratio_neg_inside)
+    neg_test_outside = negative_sampling(edge_index = data_test.edge_index,
+                                        num_neg_samples = int(data_test.edge_index.size(1) * (1 - ratio_neg_inside)))
+
+    test_pos_edge_index = data_test.edge_index
+    test_neg_edge_index = torch.cat(negs_test + [neg_test_outside], dim=1)
+    test_edge_label_index = torch.cat([test_pos_edge_index, test_neg_edge_index], dim=1)
+    test_edge_label = torch.cat([
+        torch.ones(test_pos_edge_index.size(1)),
+        torch.zeros(test_neg_edge_index.size(1))
+    ])
+    test_data = Data(
+        edge_index = test_pos_edge_index,              
+        edge_label_index = test_edge_label_index,      
+        edge_label = test_edge_label,
+        num_nodes = data_test.num_nodes,                
+    )
 
 
 if settings["options"]["attributes"] == "unique":
@@ -140,13 +408,28 @@ elif settings["options"]["attributes"] == "statistics":
     train_data.x = torch.stack([degree_tensor, clustering_tensor], dim=-1)
     val_data.x = train_data.x.clone()
     test_data.x = train_data.x.clone()
-
-
-
-#train_data = train_data.to(device)
-#val_data = val_data.to(device)
-#test_data = test_data.to(device)
-
+elif settings["options"]["attributes"] == "statistics_all":
+    neg_edges = torch.cat([train_data.edge_label_index[:, train_data.edge_label == 0], val_data.edge_label_index[:, val_data.edge_label == 0], test_data.edge_label_index[:, test_data.edge_label == 0]], dim = 1)
+    G.add_edges_from(neg_edges.t().tolist())
+    degree = dict(G.degree())
+    max_degree = max(degree.values())
+    degree_norm = {n: d/max_degree for n, d in degree.items()}
+    clustering = nx.clustering(G)
+    degree_tensor = torch.tensor(list(degree_norm.values()), dtype=torch.float32)
+    clustering_tensor = torch.tensor(list(clustering.values()), dtype=torch.float32)
+    train_data.x = torch.stack([degree_tensor, clustering_tensor], dim=-1)
+    val_data.x = train_data.x.clone()
+    test_data.x = train_data.x.clone()
+elif settings["options"]["attributes"] == "statistics_allnon":
+    degree = dict(G.degree())
+    max_degree = max(degree.values())
+    degree_norm = {n: d/max_degree for n, d in degree.items()}
+    clustering = nx.clustering(G)
+    degree_tensor = torch.tensor(list(degree_norm.values()), dtype=torch.float32)
+    clustering_tensor = torch.tensor(list(clustering.values()), dtype=torch.float32)
+    train_data.x = torch.stack([degree_tensor, clustering_tensor], dim=-1)
+    val_data.x = train_data.x.clone()
+    test_data.x = train_data.x.clone()
 
 
 
@@ -247,7 +530,7 @@ def evaluate():
 
 
 now = datetime.now()
-output_path = settings["output"] + "LP_M2_" + str(now.day) + "-" + str(now.month) + "-" + str(now.year) + "_" + str(now.hour) + ":" + str(now.minute) + ".txt"
+output_path = settings["output"] + "LP_M2_" + str(now.day) + "-" + str(now.month) + "-" + str(now.year) + "_" + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + ".txt"
 with open(output_path, "w") as output:
     best_val_auc = 0
     limit = settings["options"]["early_stop"]
@@ -261,17 +544,19 @@ with open(output_path, "w") as output:
         val_loss, val_auc, val_ap = evaluate()
         val_aps.append(val_ap)
         val_aucs.append(val_auc)
+        print(f"Epoch {epoch:03d}, Loss: {loss:.4f}, Val Loss : {val_loss:.4f}, Val AUC: {val_auc:.4f}, Val AP: {val_ap:.4f}\n")
         output.write(f"Epoch {epoch:03d}, Loss: {loss:.4f}, Val Loss : {val_loss:.4f}, Val AUC: {val_auc:.4f}, Val AP: {val_ap:.4f}\n")
         if val_auc > best_val_auc:
             best_val_auc = val_auc
             count = 0
-            output_model = settings["output"] + "model_LP_M2_" + str(now.day) + "-" + str(now.month) + "-" + str(now.year) + "_" + str(now.hour) + ":" + str(now.minute) + "_E" + str(epoch) + ".pth"
+            output_model = settings["output"] + "model_LP_M2_" + str(now.day) + "-" + str(now.month) + "-" + str(now.year) + "_" + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + "_E" + str(epoch) + ".pth"
             torch.save(model.state_dict(), output_model)
         else:
             count =  count + 1
             if count >= limit:
                 output.write("Early stop")
                 break
+    output.write(str(time.time() - start))
     output.write("\n")
     output.write("\n")
     output.write("\n")
